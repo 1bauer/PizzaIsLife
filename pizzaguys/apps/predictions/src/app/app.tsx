886bdb487a8dc24e9c36a4b187f33bfd06927d72
kajahelena <kaja-koenig@gmx.de>
2022-06-18 17:15:13 +0200

    feat: diagramm added

--- pizzaguys/apps/predictions/src/app/app.tsx
+++ pizzaguys/apps/predictions/src/app/app.tsx
@@ -1,14 +1,40 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Button } from '@mui/material';
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
+import { BrowserRouter, Link, Router } from "react-router-dom";
 
 export function App() {
   return (
     <>
-      <NxWelcome title="predictions" />
+<Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/foo">Foo</Link>
+            <Link to="/bar">Bar</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/foo" component={Foo} />
+            <Route exact path="/bar" component={Bar} />
+          </Switch>
+        </div>
+  
       <div />
-    </>
+    <div>
+        <Button variant="contained">
+          Add to Cart
+        </Button>
+        <Button variant="contained">
+          Add to Cart
+        </Button>
+      </div>
+      <NxWelcome title="predictions" />
+      </Router>
+      </>
   );
 }
+
 
 export default App;
