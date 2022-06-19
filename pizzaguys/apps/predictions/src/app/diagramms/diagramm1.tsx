eda98bff19870a7aeffe447dd64049cc6f7a0b32
kajahelena <kaja-koenig@gmx.de>
2022-06-19 12:02:33 +0200

    feat: diagramm 1+2

--- pizzaguys/apps/predictions/src/app/diagramms/diagramm1.tsx
+++ pizzaguys/apps/predictions/src/app/diagramms/diagramm1.tsx
@@ -1,80 +1,121 @@
-import { useState, useEffect } from 'react';
+
+import { useState, useEffect, Component } from 'react';
 import ReactECharts from 'echarts-for-react'; // or var ReactECharts = require('echarts-for-react');
 import Diagrammdata from '../../interfaces/diagramm1';
+import React from 'react';
 
-export interface State {
-  data: Diagrammdata[] | undefined;
-}
-const option = {
-  xAxis: {
-    type: 'category',
-    boundaryGap: false,
+
+type MyProps = {
+  // using `interface` is also ok
+
+};
+type MyState = {
+  data: Diagrammdata[]; // like this
+};
+class Diagramm1 extends React.Component<MyProps, MyState> {
+  override state: MyState = {
+    // optional second annotation for better type inference
     data: [],
-  },
-  yAxis: {
-    type: 'value',
-  },
-  series: [],
-};
-const series = {
-  data: [],
-  type: 'line',
-  areaStyle: {},
-  category: '',
-};
+  };
 
-export function Diagramm1() {
-  option.series;
-
-  const [appState, setAppState] = useState<State>({
-    data: undefined,
-  });
-
-  useEffect(() => {
-    // setAppState({ loading: true });
+  override componentDidMount() {
     const apiUrl = `assets/diagramm1.json`;
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((repos: Diagrammdata[]) => {
-        setAppState({ data: repos });
-      });
-  }, [setAppState]);
+        fetch(apiUrl)
+          .then((res) => res.json())
+          .then((repos: Diagrammdata[]) => {
+            this.setState({ data: repos });
+          });
+  }
 
-  if (appState.data != undefined) {
-    const dates = [''];
 
-    console.log(appState.data);
 
-    appState.data.forEach((element) => {
+  getSeries(data: Diagrammdata[]){
+    
+    //group by topic 
+    //get topic 
+  
+    const seriesPerTopic:any[]= [];
+    data.forEach((e) => {
+      const topicExists = seriesPerTopic.find(x => x.topic === e.topic );
+      if(topicExists === undefined){
+        seriesPerTopic.push(
+          {
+          name: e.topic,
+          data: [e.views],
+          type: 'line',
+          areaStyle: {},
+          category: e.topic,
+          topic: e.topic
+            
+          }
+        )
+      }
+      else{
+        topicExists.data.push(e.views);
+      }
+    })
+    return seriesPerTopic; 
+  }
+
+
+
+  getCategories(data: Diagrammdata[]): string[] {
+    const dates: string[] = [];
+    data.forEach((element) => {
       if (dates.indexOf(element.date) == -1) {
         dates.push(element.date);
       }
     });
+    
+    return dates;
+  }
 
-    dates.forEach((x) => {
-      option.xAxis.data.push(x);
-    });
+  override render() {
 
-    appState.data.forEach((element) => {
-      let topic = option.series.find((x) => x.category === element.topic);
-      if (topic === undefined) {
-        option.series.push({
-          data: [],
-          type: 'line',
-          areaStyle: {},
-          category: 'as',
-        });
-        topic = option.series.find((x) => x.category === element.topic);
-      }
-      topic.data.push(element.views);
-    });
-    series.data.push();
-    option.series.push();
+   if(this.state.data != undefined){
+    const options = {
+      xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: this.getCategories(this.state.data),
+      },
+    legend: {
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: this.getSeries(this.state.data),}
 
-    return <ReactECharts option={option} />;
-  } else {
-    return <></>;
+    return (
+      <div>
+        <ReactECharts option={options} />
+      </div>
+    );
+
+   }
+
+    
+    return (
+      <div>
+        {/* <ReactECharts option={option} /> */}
+      </div>
+    );
+  }
+  getSeriesNames(data: Diagrammdata[]) : string[]{
+           
+      //group by topic 
+      //get topic 
+    
+      const topics:string[]= [];
+      data.forEach((e) => {
+        const topicExists = topics.find(x => x === e.topic );
+        if(topicExists === undefined){
+          topics.push(e.topic)
+        }
+      })
+      return topics; 
   }
 }
+
 
 export default Diagramm1;
--- pizzaguys/apps/predictions/src/app/diagramms/diagramm2.tsx
+++ pizzaguys/apps/predictions/src/app/diagramms/diagramm2.tsx
@@ -1,163 +1,120 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Component } from 'react';
 import ReactECharts from 'echarts-for-react'; // or var ReactECharts = require('echarts-for-react');
+import Diagrammdata from '../../interfaces/diagramm1';
+import React from 'react';
 
-export function Diagramm2() {
-  const options = {
-    color: ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00'],
-    title: {
-      text: 'Gradient Stacked Area Chart',
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross',
-        label: {
-          backgroundColor: '#6a7985',
-        },
-      },
-    },
-    legend: {
-      data: ['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5'],
-    },
-    toolbox: {
-      feature: {
-        saveAsImage: {},
-      },
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true,
-    },
-    xAxis: [
-      {
-        type: 'category',
-        boundaryGap: false,
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      },
-    ],
-    yAxis: [
-      {
-        type: 'value',
-      },
-    ],
-    series: [
-      {
-        name: 'Line 1',
-        type: 'line',
-        stack: 'Total',
-        smooth: true,
-        lineStyle: {
-          width: 0,
-        },
-        showSymbol: false,
-        areaStyle: {
-          opacity: 0.8,
-          color: 'red',
-        },
-        emphasis: {
-          focus: 'series',
-        },
-        data: [140, 232, 101, 264, 90, 340, 250],
-      },
-      {
-        name: 'Line 2',
-        type: 'line',
-        stack: 'Total',
-        smooth: true,
-        lineStyle: {
-          width: 0,
-        },
-        showSymbol: false,
-        areaStyle: {
-          opacity: 0.8,
-          color: 'blue',
-        },
-        emphasis: {
-          focus: 'series',
-        },
-        data: [120, 282, 111, 234, 220, 340, 310],
-      },
-      {
-        name: 'Line 3',
-        type: 'line',
-        stack: 'Total',
-        smooth: true,
-        lineStyle: {
-          width: 0,
-        },
-        showSymbol: false,
-        areaStyle: {
-          opacity: 0.8,
-          color: 'yellow',
-        },
-        emphasis: {
-          focus: 'series',
-        },
-        data: [320, 132, 201, 334, 190, 130, 220],
-      },
-      {
-        name: 'Line 4',
-        type: 'line',
-        stack: 'Total',
-        smooth: true,
-        lineStyle: {
-          width: 0,
-        },
-        showSymbol: false,
-        areaStyle: {
-          opacity: 0.8,
-          color: 'green',
-        },
-        emphasis: {
-          focus: 'series',
-        },
-        data: [220, 402, 231, 134, 190, 230, 120],
-      },
-      {
-        name: 'Line 5',
-        type: 'line',
-        stack: 'Total',
-        smooth: true,
-        lineStyle: {
-          width: 0,
-        },
-        showSymbol: false,
-        label: {
-          show: true,
-          position: 'top',
-        },
-        areaStyle: {
-          opacity: 0.8,
-          color: 'purple',
-        },
-        emphasis: {
-          focus: 'series',
-        },
-        data: [220, 302, 181, 234, 210, 290, 150],
-      },
-    ],
+
+type MyProps = {
+  // using `interface` is also ok
+
+};
+type MyState = {
+  data: Diagrammdata[]; // like this
+};
+class diagramm2 extends React.Component<MyProps, MyState> {
+  override state: MyState = {
+    // optional second annotation for better type inference
+    data: [],
   };
 
-  const [appState, setAppState] = useState({
-    loading: false,
-    repos: null,
-  });
+  override componentDidMount() {
+    const apiUrl = `assets/diagramm1.json`;
+        fetch(apiUrl)
+          .then((res) => res.json())
+          .then((repos: Diagrammdata[]) => {
+            this.setState({ data: repos });
+          });
+  }
 
-  useEffect(() => {
-    // setAppState({ loading: true });
-    const apiUrl = `assets/diagramm1.csv`;
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((repos) => {
-        setAppState({ loading: false, repos: repos });
-      });
-  }, [setAppState]);
 
-  fetch('assets/diagramm1.csv').then();
 
-  return <ReactECharts option={options} />;
+  getTimeSpend(data: Diagrammdata[]){
+    
+    //group by topic 
+    //get topic 
+  
+    const seriesPerViews:any[]= [];
+    data.forEach((e) => {
+      const topicExists = seriesPerViews.find(x => x.topic === e.topic );
+      if(topicExists === undefined){
+        seriesPerViews.push(
+          {
+            name: e.topic,
+          data: [e.views],
+          type: 'line',
+          areaStyle: {},
+          category: e.topic,
+          topic: e.topic
+            
+          }
+        )
+      }
+      else{
+        topicExists.data.push(e.timespend_in_sec);
+      }
+    })
+    return seriesPerViews; 
+  }
+
+
+
+  getCategories(data: Diagrammdata[]): string[] {
+    const dates: string[] = [];
+    data.forEach((element) => {
+      if (dates.indexOf(element.date) == -1) {
+        dates.push(element.date);
+      }
+    });
+    
+    return dates;
+  }
+
+  override render() {
+
+   if(this.state.data != undefined){
+    const options = {
+      xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: this.getCategories(this.state.data),
+      },
+    legend: {
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: this.getTimeSpend(this.state.data),}
+
+    return (
+      <div>
+        <ReactECharts option={options} />
+      </div>
+    );
+
+   }
+
+    
+    return (
+      <div>
+        {/* <ReactECharts option={option} /> */}
+      </div>
+    );
+  }
+  getSeriesNames(data: Diagrammdata[]) : string[]{
+           
+      //group by topic 
+      //get topic 
+    
+      const topics:string[]= [];
+      data.forEach((e) => {
+        const topicExists = topics.find(x => x === e.topic );
+        if(topicExists === undefined){
+          topics.push(e.topic)
+        }
+      })
+      return topics; 
+  }
 }
 
-export default Diagramm2;
+
+export default diagramm2; 
